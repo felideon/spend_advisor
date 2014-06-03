8ce9b03bad7dbaaff1fa84787797574dcdf0765a
Felipe Delgado <felideon@gmail.com>
2014-06-03 03:32:58 +0000

    implement frequency_to_weekdates utility method

--- lib/utils.rb
+++ lib/utils.rb
@@ -8,4 +8,26 @@ module Utils
     JSON.parse(response.body)
   end
 
+  def frequency_to_weekdates(freq, start_date, end_date)
+    dates = []
+    num_intervals = nil
+
+    case freq[:interval]
+    when 'day'
+      num_intervals = (end_date - start_date).to_i
+    when 'week'
+      num_intervals = ((end_date - start_date).to_i / 7)
+    when 'month'
+      num_intervals = ((end_date - start_date).to_i / 30)
+    when 'year'
+      num_intervals = ((end_date - start_date).to_i / 365)
+    end
+
+    dates << (0..num_intervals).step(freq[:units]).map do |i|
+      start_date + i.send(freq[:interval])
+    end
+
+    # return dates in ISO 8601 week date format
+    dates.flatten.map { |d| d.strftime("%G-W%V-%u") }
+  end
 end
